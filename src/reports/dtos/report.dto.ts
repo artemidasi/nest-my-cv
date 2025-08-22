b8dd674f2db0bc42890e84eb55362eb93c908bcf
artemida <maxartem0419@gmail.com>
2025-08-22 17:55:11 +0300

    update reports

---
+++ src/reports/dtos/report.dto.ts
@@ -0,0 +1,36 @@
+import { Expose, Transform } from 'class-transformer';
+
+import { User } from '../../users/user.entity';
+
+export class ReportDto {
+  @Expose()
+  id: number;
+
+  @Expose()
+  price: number;
+
+  @Expose()
+  make: string;
+
+  @Expose()
+  model: string;
+
+  @Expose()
+  year: number;
+
+  @Expose()
+  lng: number;
+
+  @Expose()
+  lat: number;
+
+  @Expose()
+  mileage: number;
+
+  @Expose()
+  email: string;
+
+  @Transform(({ obj }) => obj.user.id)
+  @Expose()
+  userId: number;
+}
--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,9 +1,25 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Patch,
+  Param,
+  UseGuards,
+} from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 
 import { ReportsService } from './reports.service';
 
 import { AuthGuard } from '../guards/auth.guard';
+
+import { CurrentUser } from '../users/decorators/current-user.decorator';
+
+import { User } from '../users/user.entity';
+
+import { ReportDto } from './dtos/report.dto';
+
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { ApproveReportDto } from './dtos/approve-report.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -11,7 +27,11 @@ export class ReportsController {
 
   @Post()
   @UseGuards(AuthGuard)
-  createReport(@Body() body: CreateReportDto) {
-    return this.reportsService.create(body);
+  @Serialize(ReportDto)
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user);
   }
+
+  @Patch('/:id')
+  approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {}
 }
